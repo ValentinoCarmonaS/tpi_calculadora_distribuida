047e57730c46ed5acf12e57039df59d50b82ce56
Valentino Carmona <vcarmona@fi.uba.ar>
2025-09-11 18:50:30 -0300

    refact(client): separate the code in more functions

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,15 +1,9 @@
-#[path = "./../response.rs"]
-mod response;
-#[path = "./../errors.rs"]
-mod errors;
+use tpi_calculadora_distribuida::{errors::CalculatorErrors, response::Response};
 
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpStream;
-
-use crate::errors::CalculatorErrors;
-use crate::response::Response;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,16 +12,23 @@ fn main() {
         return Response::Error(CalculatorErrors::ArgsLenFailure).eprint();
     }
 
-    let mut stream = TcpStream::connect(&args[1]).unwrap();
+    let stream = match TcpStream::connect(&args[1]) {
+        Ok(stream) => stream,
+        Err(_) => return Response::Error(CalculatorErrors::SocketFailure).eprint(),
+    };
 
-    let file = match File::open(&args[2]) {
+    read_file(&args[2], stream);
+}
+
+fn read_file(path: &str, mut stream: TcpStream) {
+    let file = match File::open(path) {
         Ok(file) => file,
         Err(_) => return Response::Error(CalculatorErrors::FileOpenFailure).eprint(),
     };
 
-    let reader = BufReader::new(file);
+    let reader_file = BufReader::new(file);
 
-    for _line in reader.lines() {
+    for _line in reader_file.lines() {
         let line = match _line {
             Ok(line) => line,
             Err(_) => {
@@ -36,9 +37,31 @@ fn main() {
             }
         };
 
-        match stream.write_all(line.as_bytes()) {
-            Ok(_) => (),
-            Err(_) => Response::Error(CalculatorErrors::WritingFailure).eprint(),
-        };
+        send_request(&mut stream, line);
+        read_response(&mut stream); // <------------ solo deve leer al final, sacar.
     }
+
+    send_request(&mut stream, "GET".to_owned());
+    read_response(&mut stream);
+}
+
+fn send_request(stream: &mut TcpStream, data: String) {
+    if let Err(_) = stream.write_all(format!("{}\n", data).as_bytes()) {
+            return Response::Error(CalculatorErrors::WritingFailure).eprint();
+        };
+
+        if let Err(_) = stream.flush() {
+            return Response::Error(CalculatorErrors::WritingFailure).eprint();
+        };
+}
+
+fn read_response(stream: &mut TcpStream) {
+    let mut reader = BufReader::new(stream);
+    let mut response = String::new();
+    
+    if let Err(_) = reader.read_line(&mut response) {
+        return Response::Error(CalculatorErrors::ListeningFailure).eprint();
+    };
+    
+    println!("{}", response)
 }
