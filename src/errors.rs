36697e101cdbc4cc8a18617c7bb71f852bb39ef6
Valentino Carmona <vcarmona@fi.uba.ar>
2025-09-11 15:09:06 -0300

    feat(errors): add more errores

--- src/errors.rs
+++ src/errors.rs
@@ -11,17 +11,35 @@ pub enum CalculatorErrors {
     LockFailure,
     /// A string failed to parse into an operation
     ParseFailure,
+    /// A failure in writing the message
+    WritingFailure,
+    /// A failure in listening the message
+    ListeningFailure,
+    /// A failure in open the socket
+    SocketFailure,
+    /// A failure in open the file
+    FileOpenFailure,
+    /// A failure in read the line in the file
+    ReadLineFailure,
+    /// A failure in lenght of arguments
+    ArgsLenFailure,
 }
 
 impl CalculatorErrors {
     /// Prints the error message to the console
-    pub fn display(&self) {
+    pub fn get_message(&self) -> &str {
         match self {
-            Self::InvalidOperation => eprintln!("ERROR \"Operacion invalida\""),
-            Self::DivisionByZero => eprintln!("ERROR \"Division por cero\""),
-            Self::JoinFailure => eprintln!("ERROR \"Fallo en el Join\""),
-            Self::LockFailure => eprintln!("ERROR \"Fallo en el Lock\""),
-            Self::ParseFailure => eprintln!("ERROR \"Fallo en el Parseo\""),
+            Self::InvalidOperation => return "ERROR \"Operacion invalida\"",
+            Self::DivisionByZero => return "ERROR \"Division por cero\"",
+            Self::JoinFailure => return "ERROR \"Fallo en el Join\"",
+            Self::LockFailure => return "ERROR \"Fallo en el Lock\"",
+            Self::ParseFailure => return "ERROR \"Fallo en el Parseo\"",
+            Self::WritingFailure => return "ERROR \"Fallo la escritura\"",
+            Self::ListeningFailure => return "ERROR \"Fallo la lectura\"",
+            Self::SocketFailure => return "ERROR \"Fallo el Socket\"",
+            Self::FileOpenFailure => return "ERROR \"Fallo la apertura del archivo\"",
+            Self::ReadLineFailure => return "ERROR \"Fallo la lectura de la linea\"",
+            Self::ArgsLenFailure => return "ERROR \"Fallo la cantidad de argumentos\"",
         }
     }
 }
